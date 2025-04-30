d713cc89154002b7fc210942e5fbe0a748dd6f20
Triliam <taisbuenovidotto@gmail.com>
2025-04-30 17:42:06 -0300

    order class, Instant, ISO 8601, @JsonIgnore

---
+++ src/main/java/com/tais/spring_course/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.tais.spring_course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tais.spring_course.entities.Order;
+import com.tais.spring_course.entities.User;
+import com.tais.spring_course.repositories.OrderRepository;
+import com.tais.spring_course.repositories.UserRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
